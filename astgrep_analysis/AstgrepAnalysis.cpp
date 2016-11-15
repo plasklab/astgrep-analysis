cead919d7d9fd660673972db542e11cb2ae2c8f6
tanishiking24 <rikiriki1238@gmail.com>
2016-11-15 17:18:48 +0900

    Get all clobbering instructions

--- astgrep_analysis/AstgrepAnalysis.cpp
+++ astgrep_analysis/AstgrepAnalysis.cpp
@@ -4,8 +4,12 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Transforms/Utils/MemorySSA.h"
 #include "llvm/Analysis/MemoryDependenceAnalysis.h"
+#include <set>
+#include <map>
 
 using namespace llvm;
+
+typedef std::set<Instruction*>* InstSet;
 
 namespace {
   class AstgrepPass : public FunctionPass {
@@ -16,6 +20,10 @@ namespace {
 
     virtual bool runOnFunction(Function &F);
     virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+
+  private:
+    bool upAndMark(Instruction &inst, Instruction &liveInst);
+    InstSet clobberingMemoryInstSet(MemoryAccess* MA, MemorySSAWalker* walker);
   };
 }
 
@@ -25,37 +33,95 @@ void AstgrepPass::getAnalysisUsage(AnalysisUsage &AU) const {
 }
 
 bool AstgrepPass::runOnFunction(Function &F) {
+  // instruction の前後で生存しているinstructionを保存(メモリに関するものだけ)
+  // instruction じゃなっくて変数まで剥いてやったほうがはやそう
+  // instruction からdubugLocを取得できる
+  std::map<Instruction*, InstSet> instLiveBefore;
+  std::map<Instruction*, InstSet> instLiveAfter;
+
   MemorySSA* MSSA = &getAnalysis<MemorySSAWrapperPass>().getMSSA();
+  MemorySSAWalker* walker = MSSA->getWalker();
+
+  // want to traverse all memory allocated variables
+  // TODO: how to get all memory allocated variables?
+  //  - Get all MemoryAccess by traversing and casting (to MA) instructions
+  //  - get information from Basic Alias Analysis?
 
   // get all instruction in the Function
   std::list<Instruction *> works;
   for (inst_iterator inst_it = inst_begin(F); inst_it != inst_end(F); inst_it++) {
     works.push_back(&*inst_it);
   }
-  while (!works.empty()) {
-    Instruction* inst = works.front();
-    works.pop_front();
 
+  while (!works.empty()) {
+    Instruction* inst = works.back();
+    works.pop_back();
     MemoryAccess* MA = MSSA->getMemoryAccess(inst);
-    inst->dump();
     if (MA != nullptr) {
-      for (memoryaccess_def_iterator defs_it = MA->defs_begin(); defs_it != MA->defs_end(); defs_it++) {
-        // errs() << "def: " << **defs_it << "\n";
-        MemoryUseOrDef* memoryUseOrDef = dyn_cast<MemoryUseOrDef>(*defs_it);
-        if (memoryUseOrDef != nullptr) {
-          Instruction* instruction = memoryUseOrDef->getMemoryInst();
-          if (instruction != nullptr) {
-            instruction->dump();
-            if(instruction->hasMetadata()) {
-              instruction->getDebugLoc().dump();
-            }
-          }
-        }
+      if (!isa<MemoryUse>(MA)) {
+        continue;
       }
+
+      InstSet clobberingInsts = this->clobberingMemoryInstSet(MA, walker);
+      for (auto inst = clobberingInsts->begin(); inst != clobberingInsts->end(); inst++) {
+        errs() << "[clobbering]";
+        (*inst)->dump();
+      }
+
+      // if memory access is MemoryUse
+      // propagate live information of this instruction forward
+      // MemoryUse* thisMemoryUse = dyn_cast<MemoryUse>(MA);
+      // Instruction* thisInstruction = thisMemoryUse->getMemoryInst();
+      // insert clobbering instruction
+      // instLiveBefore[thisInstruction].insert()
     }
-    errs() << "\n";
+    inst->dump();
   }
   return false;
+}
+
+bool AstgrepPass::upAndMark(Instruction &inst, Instruction &liveInst) {
+  return false;
+}
+
+InstSet AstgrepPass::clobberingMemoryInstSet(MemoryAccess* MA, MemorySSAWalker* walker) {
+  // get all instruction of clobbering memory access
+
+  InstSet instSet = new std::set<Instruction*>();
+
+  // clobberingMemoryAccess should be MemoryPhi or MemoryDef
+  // MemoryAccess* clobbering = walker->getClobberingMemoryAccess(MA);
+  MemoryUseOrDef* memoryUseOrDef = dyn_cast<MemoryUseOrDef>(MA);
+  MemoryPhi* memoryPhi = dyn_cast<MemoryPhi>(MA);
+
+  /*
+  if (instruction != nullptr && instruction->hasMetadata()) {
+    instruction->getDebugLoc().dump();
+  }
+  */
+  if (memoryUseOrDef != nullptr) {
+    if (isa<MemoryDef>(memoryUseOrDef)) {
+      Instruction* instruction = memoryUseOrDef->getMemoryInst();
+      instSet->insert(instruction);
+      return instSet;
+    } else if (isa<MemoryUse>(memoryUseOrDef)) {
+      MemoryAccess* clobbering = walker->getClobberingMemoryAccess(MA);
+      InstSet clobbringInstSet = this->clobberingMemoryInstSet(clobbering, walker);
+      for (auto inst = clobbringInstSet->begin(); inst != clobbringInstSet->end(); inst++) {
+        instSet->insert(*inst);
+      }
+    }
+  } else if (memoryPhi != nullptr) {
+    for (unsigned int i = 0; i < memoryPhi->getNumOperands(); i++) {
+      // get all incoming memory access
+      MemoryAccess* incomingMA = memoryPhi->getIncomingValue(i);
+      InstSet clobbringInstSet = this->clobberingMemoryInstSet(incomingMA, walker);
+      for (auto inst = clobbringInstSet->begin(); inst != clobbringInstSet->end(); inst++) {
+        instSet->insert(*inst);
+      }
+    }
+  }
+  return instSet;
 }
 
 char AstgrepPass::ID = 0;
